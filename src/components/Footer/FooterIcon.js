030966d16ccf912a59c3ff81ec8af17c11cd675c
Nick Landreth <nick.landreth@nielsen.com>
2022-09-04 05:21:18 -0700

    fix: disable icon links in admin

--- src/components/Footer/FooterIcon.js
+++ src/components/Footer/FooterIcon.js
@@ -1,31 +1,36 @@
-import React , {useState, useEffect} from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faFacebookSquare} from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import React, { useState, useEffect } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons"
 
-export default function FooterIcon ({icon, link}){
-    const [iconType, setIconType] = useState()
-    useEffect(() =>{
-        switch(icon){
-            case "facebook":
-                setIconType(faFacebookSquare)
-                break
-            case "email":
-                setIconType(faEnvelope)
-                break
-            default:
-                setIconType(null)
-        }
-    },[])
-    
+export default function FooterIcon({ icon, link }) {
+  const [iconType, setIconType] = useState()
+  useEffect(() => {
+    switch (icon) {
+      case "facebook":
+        setIconType(faFacebookSquare)
+        break
+      case "email":
+        setIconType(faEnvelope)
+        break
+      default:
+        setIconType(null)
+    }
+  }, [])
 
-    return(
-        <>
+  return (
+    <>
+      {!currentUser || location.pathname.slice(1, 5) !== "edit" ? (
+        <FontAwesomeIcon className="footer-icon p-1 mx-5" icon={iconType}>
+          {" "}
+        </FontAwesomeIcon>
+      ) : (
         <a href={link} className="footer-icon">
-            <FontAwesomeIcon className="footer-icon p-1 mx-5" icon={iconType} > </FontAwesomeIcon>
-            </a>
-        </>
-
-    )
-
+          <FontAwesomeIcon className="footer-icon p-1 mx-5" icon={iconType}>
+            {" "}
+          </FontAwesomeIcon>
+        </a>
+      )}
+    </>
+  )
 }
